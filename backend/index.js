6fe9ad7214e552e91bdd446b2d69c891375f7c7e
jadedrb <jadecdrb@gmail.com>
2025-07-11 11:15:12 -0400

    added a get all todos route

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,22 @@ import 'dotenv/config'
 
 import connectDb from './db.js'
 
+import Todo from './models/todo.js'
+
 const app = express()
 
 const port = process.env.PORT
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.json('Hello (from server)')
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find({})
+        res.status(200).json(todos)
+    } catch (e) {
+        console.log(e)
+        res.status(400).json(e)
+    }
 })
 
 app.listen(port, () => { 
