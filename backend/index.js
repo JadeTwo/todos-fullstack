fdd39da05dce623d3917a2525f27e908e6d9c502
jadedrb <jadecdrb@gmail.com>
2025-07-11 12:09:00 -0400

    rendering a list of todos

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,25 @@ const app = express()
 
 const port = process.env.PORT
 
+app.use(express.json())
 app.use(cors())
 
 app.get('/todos', async (req, res) => {
     try {
         const todos = await Todo.find({})
         res.status(200).json(todos)
+    } catch (e) {
+        console.log(e)
+        res.status(400).json(e)
+    }
+})
+
+app.post('/todos', async (req, res) => {
+    try {
+        // const todo = new Todo(req.body)
+        // await todo.save()
+        const todo = await Todo.create(req.body)
+        res.status(200).json(todo)
     } catch (e) {
         console.log(e)
         res.status(400).json(e)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 
   const [todos, setTodos] = useState([])
+  const inputRef = useRef()
 
   useEffect(() => {
 
@@ -12,7 +13,7 @@ function App() {
         const response = await fetch('http://localhost:8080/todos')
         const data = await response.json()
         console.log(data) 
-        setTodos(todos)
+        setTodos(data)
       } catch (e) {
         console.log(e)
       }
@@ -22,11 +23,42 @@ function App() {
 
   }, [])
 
-  console.log(todos)
+  console.log('todos: ', todos)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const todo = {
+      text: inputRef.current.value
+    }
+    
+    const response = await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const newTodo = await response.json()
+
+    console.log(newTodo)
+
+    setTodos([...todos, newTodo])
+
+  }
  
   return (
     <>
-      Hello (from frontend)
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {todos.map((todo) => 
+          <li key={todo._id}>{todo.text}</li>
+        )}
+      </ul>
     </>
   )
 }
